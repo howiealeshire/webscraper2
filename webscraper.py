cf169f9dfba83824070e4ab566b6984226e7b49e
Howie <howie@Howies-MacBook-Pro.local>
2020-07-17 12:16:06 -0400

    Generated list of urls.

--- webscraper.py
+++ webscraper.py
@@ -37,11 +37,45 @@ def get_names_and_locations(file_path):
     names_and_locs = list(zip(df.iloc[:, 0], df.iloc[:, 1]))
     return names_and_locs
 
+def gen_url(first_name, last_name, city, state):
+    test_url = "https://www.truepeoplesearch.com/details?name=John%20Smith&citystatezip=Atlanta%2C%20GA&rid=0x0"
+    template_url = "https://www.truepeoplesearch.com/details?name=" + first_name + "%20" + last_name + "&citystatezip=" + city + "%2C%20" + state +  "&rid=0x0"
+    return template_url
+
+def gen_all_urls(names_and_locs):
+    url_list = []
+
+    for elem in names_and_locs:
+        url = gen_url(elem[0],elem[1],elem[2],elem[3])
+        url_list.append(url)
+    return url_list
+
+def gen_param_list_for_urls(names_and_locs):
+    gen_list = []
+    for idx, val in enumerate(names_and_locs):
+        first_name = names_and_locs[idx][0].split()[0]
+        last_name = names_and_locs[idx][0].split()[1]
+        city = names_and_locs[idx][1].split()[0]
+        city = city.replace(',', '')
+        state = names_and_locs[idx][1].split()[1]
+        elem_to_add = [first_name,last_name, city,state]
+        gen_list.append(elem_to_add)
+    return gen_list
+
 def main():
     pass
 
+
 if __name__ == '__main__':
-    #pp = pprint.PrettyPrinter(indent=4)
+    pp = pprint.PrettyPrinter(indent=4)
     names_and_locs = get_names_and_locations("/Users/howie/Downloads/RA_List.xlsx")
-    pp.pprint(names_and_locs)
+    param_list = gen_param_list_for_urls(names_and_locs)
+    #pp.pprint(param_list)
+    x = gen_all_urls(param_list)
+    pp.pprint(x)
+
+
+
+
+
    # print("hello")
